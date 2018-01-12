254aa6b2e10437553f3e485658f6f94325dcac38
EgorPrihodko123 <gestithe@gmail.com>
2018-01-12 16:48:55 +0300

    calculator

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int num1 = getInt();
-        int num2 = getInt();
+        double num1 = getInt();
+        double num2 = getInt();
         char operation = getOperation();
-        int result = calculator(num1, num2, operation);
+        double result = calculator(num1, num2, operation);
         System.out.println("Ответ: " + result);
     }
-    public static int getInt() {
+    public static double getInt() {
         System.out.println("Введите число:");
-        int num;
-        num = scanner.nextInt();
+        double num;
+        num = scanner.nextDouble();
+        if(num=='='){
+            System.exit(0);
+        }
         return num;
     }
     public static char getOperation() {
@@ -20,8 +23,8 @@ public class Main {
         operation = scanner.next().charAt(0);
         return operation;
     }
-    public static int calculator(int num1, int num2, char operation) {
-        int result;
+    public static double calculator(double num1, double num2, char operation) {
+        double result;
         switch (operation) {
             case '+':
                 result = num1 + num2;
@@ -32,8 +35,18 @@ public class Main {
             case '*':
                 result = num1 * num2;
                 break;
+            case '=':
+                result=0;
+                System.exit(0);
+                break;
             case '/':
                 result = num1 / num2;
+                break;
+            case '%':
+                int percent;
+                System.out.println("Введите процент");
+                percent = scanner.nextInt();
+                result=(num1+num2)/100*percent;
                 break;
             default:
                 System.out.println("Операция невозможна, попробуйте еще раз:)");
